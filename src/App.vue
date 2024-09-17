ba34e18561058b96ead4fe32d35e95fcbabb4c20
Ghosterbeef <vahnin24@gmail.com>
2024-09-17 19:22:26 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="content">
+        <div class="content-group">
+            <VInput label="Поле ввода 1" placeholder="Тут"/>
+            <VInput label="Поле ввода 2" placeholder="Кроется"/>
+            <VInput label="Поле ввода 3" placeholder="Какой-то"/>
+            <VInput label="Нажми на меня" placeholder="Подвох" :messages="['Какие то странные поля ввода']"/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import VInput from "@/components/VInput.vue";
+</script>
+
+<style>
+*,*::before,*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background-color: #333;
+    font-size: 16px;
+    line-height: 20px;
+    font-family: sans-serif;
+}
+
+.content {
+    margin-inline: auto;
+    width: min(85%, 750px);
+}
+
+.content-group {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+</style>
---
+++ src/components/VInput.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="field">
+        <label v-if="label" class="field__label" :for="id">{{label}}</label>
+        <input :id="id" class="field__input" :placeholder="placeholder" :type="type">
+        <span v-for="hint in messages" :key="hint" class="field__hint">{{hint}}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    id?: string;
+    label?: string;
+    placeholder?: string;
+    type?: string;
+    messages?: string[];
+}
+
+withDefaults(defineProps<Props>(), {id: Math.random(), type: 'text', messages: []})
+</script>
+
+<style scoped>
+.field {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+
+.field__input {
+    display: block;
+    font-size: 20px;
+    line-height: 24px;
+    padding: 8px;
+    border: none;
+    border-radius: 4px;
+    outline: none;
+}
+
+.field__label {
+    color: #fff;
+}
+
+.field__hint {
+    color: #d5d5d5;
+}
+</style>
